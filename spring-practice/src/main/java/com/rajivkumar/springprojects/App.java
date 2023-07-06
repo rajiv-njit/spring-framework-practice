329cd94d5ce2af8284a1cc915532b51f3a271b71
Rajiv Kumar <rajivk.cse2005@gmil.com>
2023-07-05 20:25:18 -0400

    Initial Spring setup project

---
+++ spring-practice/src/main/java/com/rajivkumar/springprojects/App.java
@@ -0,0 +1,13 @@
+package com.rajivkumar.springprojects;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
